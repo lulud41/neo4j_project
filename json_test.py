f151263c2a415b5a4bd18bdd95fe416c24f38906
Antoine101 <48209504+Antoine101@users.noreply.github.com>
2022-03-15 18:55:41 +0100

    Correction de l'erreur sur les relations, ajout d'une septième requête et modification de l'introduction

---
+++ json_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 11:58:00 2022
+
+@author: antoine
+"""
+
+import json
+
+with open('dataset_18K_v7.json') as json_file:
+    data = json.load(json_file)
